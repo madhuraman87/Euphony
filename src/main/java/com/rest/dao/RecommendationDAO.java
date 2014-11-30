eb6a1fadca275a91ac7bb21c966e6787e2d01462
madhuraman87 <madhuraman87@gmail.com>
2014-11-30 00:25:58 -0800

    added UI for all recommendations in track details page

--- src/main/java/com/rest/dao/RecommendationDAO.java
+++ src/main/java/com/rest/dao/RecommendationDAO.java
@@ -5,10 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class RecommendationDAO {
 	private static final String userRecommendationTable = "usercommendation";
